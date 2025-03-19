181bf93c1ca855e0154dfeab5494db448e365ff5
Joel <joel.viana@audora.com.br>
2025-03-19 18:45:11 -0300

    configuraçoes inicias, conexao com banco de dados, instalando dependencias, docker compose

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import express from "express";
+import { AppDataSource } from "./ormconfig";
+
+const app = express();
+app.use(express.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Conectado ao banco de dados!");
+
+    app.listen(3000, () => {
+      console.log("🚀 Servidor rodando na porta 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco de dados", err);
+  });
